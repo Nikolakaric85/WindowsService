3dfb1cc16b0088c5934437e790d07e511d51e0bd
Nikolakaric85 <nikolakaricsd@gmail.com>
2021-06-08 20:47:51 +0200

    Upisuje u bazu ali ima ponavljanja mnogop

--- WindowsService/MyWindowsService.cs
+++ WindowsService/MyWindowsService.cs
@@ -10,6 +10,7 @@ using System.Timers;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml.Linq;
+using System.Data.SqlClient;
 
 namespace WindowsService
 {
@@ -39,18 +40,75 @@ namespace WindowsService
 
         private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
         {
-            if (e.Name == "note.xml")
+            if (e.Name == "Test_ok.txt")
             {
-             
-              
-                EventLog.WriteEntry("Ubačen je neki element", EventLogEntryType.Information);
+                try
+                {
+                    string conString = "Server=NIKOLA-PC\\DATALAB55;Database=test; User ID=sa;password=123";
+                    string insertQuery = "INSERT INTO test1(line1, line2) VALUES(@line1, @line2)";
+
+                    using (SqlConnection sqlConnection = new SqlConnection(conString))
+                    {
+                        SqlCommand command = new SqlCommand(insertQuery, sqlConnection);
+
+                        command.Parameters.Add("@line1", SqlDbType.NVarChar);
+                        command.Parameters.Add("@line2", SqlDbType.NVarChar);
+                        sqlConnection.Open();
+                    
+
+                        
+
+                   // EventLog.WriteEntry("Status konekcije je " + sqlConnection.State, EventLogEntryType.Information);
+
+                    string line;
+                    System.IO.StreamReader file = new System.IO.StreamReader(@"C:\temp\Test_ok.txt");
+
+                        int counter = 0;
+
+                        // da se uradi da se nadje max counter pa onda for petlja pa da tako upise u bazu da ne bi bilo ponavljanja
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        EventLog.WriteEntry("Ubačen je red " + line, EventLogEntryType.Information);
+                        command.Parameters["@line1"].Value = line;
+                        command.Parameters["@line2"].Value = DateTime.Now;
+
+                        command.ExecuteNonQuery();
+                            counter++;
+                    }
+                        EventLog.WriteEntry("Broj redova " + counter, EventLogEntryType.Warning);
+
+
+                        file.Close();
+                    }
+
+                    string destinationPath = @"C:\temp\OK\" + e.Name;
+                    File.Move(@"C:\temp\Test_ok.txt", destinationPath);
+                    EventLog.WriteEntry("Ubačen je fajl " + e.Name, EventLogEntryType.Warning);
+                }
+                catch (Exception)
+                {
+
+                    //EventLog.WriteEntry("Nije prosao proces", EventLogEntryType.Error);
+                }
+
             }
             else
             {
-                EventLog.WriteEntry("Nije Hearbeat.txt fajl", EventLogEntryType.Information);
+                try
+                {
+                    string destinationPath = @"C:\temp\Bad\" + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + " " + e.Name;
+                    File.Move(@"C:\temp\" + e.Name, destinationPath);
+                    EventLog.WriteEntry("Neispravan fajl: " + e.Name, EventLogEntryType.Information);
+                }
+                catch (Exception)
+                {
+                    EventLog.WriteEntry("Nije prosao proces", EventLogEntryType.Error);
+                }
+
             }
 
-            
+
         }
 
         protected override void OnStop()
@@ -59,3 +117,54 @@ namespace WindowsService
         }
     }
 }
+
+
+/*
+  if (e.Name == "Test_ok.txt")
+            {
+                try
+                {
+                    // string destinationPath = "C:\\temp\\OK\\note " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".xml";
+
+                    string line;
+
+                    List<string> myList = new List<string>();
+
+                    System.IO.StreamReader file = new System.IO.StreamReader(@"C:\temp\Test_ok.txt");
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        EventLog.WriteEntry("Ubačen je red " + line, EventLogEntryType.Information);
+                        myList.Add(line);
+                    }
+
+                    File.WriteAllLines(@"C:\temp\OK\note.txt", myList);
+                    file.Close();
+
+                    string destinationPath = @"C:\temp\OK\" + e.Name;
+                    File.Move(@"C:\temp\Test_ok.txt", destinationPath);
+                    EventLog.WriteEntry("Ubačen je fajl " + e.Name, EventLogEntryType.Warning);
+                }
+                catch (Exception)
+                {
+
+                    //EventLog.WriteEntry("Nije prosao proces", EventLogEntryType.Error);
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    string destinationPath = @"C:\temp\Bad\" + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + " " + e.Name;
+                    File.Move(@"C:\temp\" + e.Name, destinationPath);
+                    EventLog.WriteEntry("Neispravan fajl: " + e.Name, EventLogEntryType.Information);
+                }
+                catch (Exception)
+                {
+                    EventLog.WriteEntry("Nije prosao proces", EventLogEntryType.Error);
+                }
+
+            }
+ 
+ */
