07a8a549e3073deaea4de1134cda101c4ef3fae3
Nikola Karić <nikolakaricsd@gmail.com>
2021-06-11 15:53:36 +0200

    posao 11.06.2021

--- WindowsService/MyWindowsService.cs
+++ WindowsService/MyWindowsService.cs
@@ -4,7 +4,7 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.ServiceProcess;
-using System.Timers;
+using System.Threading;
 
 namespace WindowsService
 {
@@ -13,7 +13,9 @@ namespace WindowsService
         public MyWindowsService()
         {
             InitializeComponent();
-            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"C:\temp");
+           // FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda");
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"E:\wsTest");
+           // FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"C:\temp");
             fileSystemWatcher.EnableRaisingEvents = true;
             fileSystemWatcher.Created += OnCreated;
         }
@@ -28,47 +30,110 @@ namespace WindowsService
 
             if (e.Name == "Test_ok.txt")
             {
-                EventLog.WriteEntry("Kreiran je fajl " + e.Name, EventLogEntryType.Information);
+                bool success = false;
+                int retry = 0;
 
-                string conString = @"Server=192.168.2.200\SQLEXPRESS01; Database=els; User ID=elsUser; password=els5577";
-                string insertQuery = "INSERT INTO Test_wsCoreda(line1, line2) VALUES(@line1, @line2)";
-
-                using (SqlConnection sqlConnection = new SqlConnection(conString))
+                while (!success && retry < 3)
                 {
-                    using (SqlCommand command = new SqlCommand(insertQuery, sqlConnection))
+
+                    try
                     {
+                        EventLog.WriteEntry("Kreiran je fajl " + e.Name, EventLogEntryType.Information);
 
-                        command.Parameters.Add("@line1", SqlDbType.Char);
-                        command.Parameters.Add("@line2", SqlDbType.DateTime);
-                        sqlConnection.Open();
-                        EventLog.WriteEntry("Status konekcije je " + sqlConnection.State, EventLogEntryType.Warning);
+                        string conString = @"Server=192.168.2.200\SQLEXPRESS01; Database=els; User ID=elsUser; password=els5577";
+                        string insertQuery = "INSERT INTO Test_wsCoreda(line1, line2) VALUES(@line1, @line2)";
+                     
 
-                        string line;
-                        System.IO.StreamReader file = new System.IO.StreamReader(@"C:\temp\Test_ok.txt");
-
-                        while ((line = file.ReadLine()) != null)
+                        using (SqlConnection sqlConnection = new SqlConnection(conString))
                         {
-                            command.Parameters["@line1"].Value = line;
-                            command.Parameters["@line2"].Value = DateTime.Now;
-                            command.ExecuteNonQuery();
+                            using (SqlCommand command = new SqlCommand(insertQuery, sqlConnection))
+                            {
+
+                                command.Parameters.Add("@line1", SqlDbType.Char);
+                                command.Parameters.Add("@line2", SqlDbType.DateTime);
+                                sqlConnection.Open();
+                                 EventLog.WriteEntry("Status konekcije je " + sqlConnection.State, EventLogEntryType.Warning);
+
+                                string line;
+                             //  using (StreamReader file = new StreamReader(@"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda" + e.Name))
+                                // using (StreamReader file = new StreamReader(@"C:\temp\" + e.Name))
+                                 using (StreamReader file = new StreamReader(@"E:\wsTest\" + e.Name))
+                                {
+                                    while ((line = file.ReadLine()) != null)
+                                    {
+                                        command.Parameters["@line1"].Value = line;
+                                        command.Parameters["@line2"].Value = DateTime.Now;
+                                        command.ExecuteNonQuery();
+                                    }
+                                    command.Dispose();
+                                    file.Dispose();
+                                    file.Close();
+                                }
+
+                                Thread.Sleep(1000);
+                            }
+                            sqlConnection.Close();
+                            //  EventLog.WriteEntry("Status konekcije je " + sqlConnection.State, EventLogEntryType.Information);
+
                         }
-                        command.Dispose();
-                        file.Close();
+
+                        string destinationPath = @"E:\wsTest\OK\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+                        File.Move(@"E:\wsTest" + e.Name, destinationPath);
+
+                        //string destinationPath = @"C:\temp\OK\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+                        //File.Move(@"C:\temp\Test_ok.txt", destinationPath);
+
+                        success = true;
+
+
+                    }//kraj try
+
+                    catch (Exception exp)
+                    {
+                        retry++;
+
+                        if (retry >= 3)
+                        {
+                            EventLog.WriteEntry("Broj pokusaja  " + retry, EventLogEntryType.Warning);
+                            moveBadFile(e.Name);
+
+                        }
+                        EventLog.WriteEntry("Poruka  " + exp.Message, EventLogEntryType.Warning);
+
                     }
-                    sqlConnection.Close();
-                    EventLog.WriteEntry("Status konekcije je " + sqlConnection.State, EventLogEntryType.Information);
-                }
 
-                string destinationPath = @"C:\temp\OK\" + e.Name+ " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
-                File.Move(@"C:\temp\Test_ok.txt", destinationPath);
+                } //kraj while (!success && retry < 3)
+
+
             }
-
             else
             {
+                //EventLog.WriteEntry("Neispravan fajl " + e.Name, EventLogEntryType.Warning);
+                //string destinationPath = @"C:\temp\Bad\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+                //File.Move(@"C:\temp\" + e.Name, destinationPath);
+
+                //EventLog.WriteEntry("Neispravan fajl " + e.Name, EventLogEntryType.Warning);
+                //string destinationPath = @"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda\Bad\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+                //File.Move(@"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda" + e.Name, destinationPath);
+
                 EventLog.WriteEntry("Neispravan fajl " + e.Name, EventLogEntryType.Warning);
-                string destinationPath = @"C:\temp\Bad\" + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + " " + e.Name;
-                File.Move(@"C:\temp\" + e.Name, destinationPath);
+                string destinationPath = @"E:\wsTest\Bad\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+                File.Move(@"E:\wsTest\" + e.Name, destinationPath);
             }
+        }
+
+        public void moveBadFile(string name)
+        {
+            //// EventLog.WriteEntry("Broj pokusaja " + retry, EventLogEntryType.Warning);
+            //EventLog.WriteEntry("Neispravan fajl " + name, EventLogEntryType.Warning);
+            //string destinationP = @"C:\temp\Bad\" + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + " " + name;
+            //File.Move(@"C:\temp\" + name, destinationP);
+
+            //string destinationPath = @"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda\Bad\" + name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+            //File.Move(@"\\192.168.2.254\Eurolog ICT\11. Software\Test_wsCoreda" + name, destinationPath);
+
+            string destinationPath = @"E:\wsTest\Bad\" + name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+            File.Move(@"E:\wsTest\Bad\" + name, destinationPath);
         }
 
         protected override void OnStop()
@@ -80,7 +145,14 @@ namespace WindowsService
 
 /*
     string conString = @"Server=192.168.2.200\SQLEXPRESS01; Database=els; User ID=elsUser; password=els5577";
-   string insertQuery = "INSERT INTO Test_wsCoreda(line1, line2) VALUES(@line1, @line2)";
+    string insertQuery = "INSERT INTO Test_wsCoreda(line1, line2) VALUES(@line1, @line2)";
+
+
+    string destinationPath = @"C:\temp\OK\" + e.Name + " " + DateTime.Now.ToString("dddd, dd MMMM yyyy, HH mm ss") + ".txt";
+    File.Move(@"C:\temp\Test_ok.txt", destinationPath);
+
+
+
  */
 
 
